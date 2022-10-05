cd1a2ca0de9f9fe3cad9cc38b14d0a1345432748
Akshay Kumbhare <52664007+Akshay7-max@users.noreply.github.com>
2022-10-05 21:30:55 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project2';
+
+  originalemployees: Employee[] = [
+    new Employee(1, 'Ram', 4000),
+    new Employee(2, 'Shyam', 5000),
+    new Employee(3, 'Krishna', 6000),
+    new Employee(4, 'Hari', 7000)
+  ];
+
+  employees: Employee[] = [];
+
+  constructor() {
+    this.employees = this.originalemployees;
+  }
+
+  str: string = '';
+  sortcolumn = 'empid';
+  order = 1;
+
+  onSearchClick() {
+    this.employees = this.originalemployees.filter((emp:any) => {
+      return emp.empname.toLowerCase().indexOf(this.str.toLowerCase()) >= 0;
+    });
+  }
+
+  onSortClick() {
+    this.employees = this.originalemployees.sort((emp1:any, emp2:any) => {
+      var n = 0;
+      if (this.sortcolumn == 'empid') {
+        return (emp1[this.sortcolumn] - emp2[this.sortcolumn]) * this.order;
+      } else if (this.sortcolumn == 'empname') {
+        return (
+          emp1[this.sortcolumn].charCodeAt(0) -
+          emp2[this.sortcolumn].charCodeAt(0)) *
+          this.order
+      } else {
+        return (emp1[this.sortcolumn] - emp2[this.sortcolumn]) * this.order;
+      }
+    });
+  }
 }
